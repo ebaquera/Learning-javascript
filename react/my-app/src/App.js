ccd46c0be015376a82cbab992074abffce53ac36
ebaquera <edna.baquera@gmail.com>
2021-07-12 10:37:48 -0700

    install react app

---
+++ react/my-app/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="Test1">
+      <header className="App-header">
+        <p>
+          this is my first try at react - edna
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
